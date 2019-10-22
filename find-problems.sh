8e2febd62d31566d70d5ad070a1112729b7e199f
Sergei Miami <miami@blackcrystal.net>
2019-10-23 00:02:03 +0300

    Added most bushy folders

--- find-problems.sh
+++ find-problems.sh
@@ -6,10 +6,16 @@ cd $SCRIPTDIR
 
 echo "Hello."
 echo ""
-echo "This is analysis of account $(whoami) at $(hostname)."
+echo "This is overview of account $(whoami) at $(hostname)."
 echo ""
 echo "[PHP files modified today]"
 find -name "*.php" -mtime -1 -printf "%T@ %Tb %Td %TY %p\n" | sort -n
+
+echo ""
+echo "[Most bushy folders]"
+find -type d -print0 2>/dev/null | while IFS= read -r -d '' dir; do 
+    echo -e `ls -A "$dir" 2>/dev/null | wc -l` "files in $dir"
+done | sort -nr | head
 
 echo ""
 echo "[Files containing suspicious words]"
